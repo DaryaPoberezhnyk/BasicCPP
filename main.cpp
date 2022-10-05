109fc297b05caab5b570ef1b3df8c1675c0a6bd2
DashaPoberezhnik <dasosha2017@gmail.com>
2022-10-05 22:44:47 +0300

    Done 22.09.22 for HW

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
-int main()
+float main()
 {
-	return 0;
+	float R1;
+	float R2;
+	float R3;
+	float R0;
+
+	cout << "Enter R1 : " << endl;
+	cin >> R1;
+	cout << "Enter R2 : " << endl;
+	cin >> R2;
+	cout << "Enter R3 : " << endl;
+	cin >> R3;
+
+
+	R0 = 1 / ((1 / R1) + (1 / R2) + (1 / R3));
+	cout << "R0 = " << R0;
 }
