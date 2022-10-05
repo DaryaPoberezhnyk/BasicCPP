d9738d976ef4f2dba81d5307abbd506177f5a086
DashaPoberezhnik <dasosha2017@gmail.com>
2022-10-06 01:17:15 +0300

    Done 06.10.22 for HW

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
+    cout << "\t\t\t#>---------<MENU>---------<#" << endl;
+    cout << "\t\t\t|        1 - Case 1        |" << endl;
+    cout << "\t\t\t|        2 - Case 2        |" << endl;
+    cout << "\t\t\t|        3 - Case 3        |" << endl;
+    cout << "\t\t\t#>-------------------------#" << endl;
+    cout << "\t\t\t|          Exit - 0        |" << endl;
+    cout << "\t\t\t#>------------------------<#" << endl;
+    cout << "\t\t\tEnter task =>";
+    int number;
+    cin >> number;
+    system("cls");
 
 	return 0;
 }
