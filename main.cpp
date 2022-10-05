eafce442aa7d7bb317781381e89af20cb2923809
DashaPoberezhnik <dasosha2017@gmail.com>
2022-10-05 22:50:46 +0300

    Done 22.09.22(2) for HW

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
-int main()
+float main()
 {
-	return 0;
+	const double pi = 3.14;
+	float S;
+	float L;
+	float R;
+
+	cout << "Enter the lengh of the circumference: ";
+	cin >> L;
+
+	R = L / 2 * pi;
+	L = 2 * pi * R;
+	S = pi * (R * R);
+
+	cout << "S = " << S << endl;
+	cout << "R = " << R << endl;
 }
