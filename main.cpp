05d9a41aa8df8765e73c0a10e825f38703a9fa5f
DashaPoberezhnik <dasosha2017@gmail.com>
2022-10-05 22:40:45 +0300

    Done 16.09.22 for HW

--- main.cpp
+++ main.cpp
@@ -3,5 +3,45 @@ using namespace std;
 
 int main()
 {
+	cout << endl;
+	cout << endl;
+	cout << "    Name\t" << "        : \"The war and the peace" << endl;
+	cout << "    Avtor\t        : L.N. Tolstoj" << endl;
+	cout << "    Izdatelstvo\t        : Piter" << endl;
+	cout << "    Pages\t        : 500." << endl;
+	cout << endl << endl << endl << endl;
+	cout << "    \"Kraj lukomor’a dub zelenyj,""\a" << endl;
+	cout << "    I zolotyj lancyuh na nim:""\a" << endl;
+	cout << "    Shodnya, shonochi kit uchenyj""\a" << endl;
+	cout << "    Na lancyuhu kruzhl’aje tim\"""\a" << endl;
+	cout << endl << endl << endl << endl;
+	cout << "    " << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(187) << endl;
+	cout << endl;
+	cout << "    " << char(186) << "                  Pory Roku                   " << char(186) << endl;
+	cout << endl;
+	cout << "    " << char(204) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(185) << endl;
+	cout << endl;
+	cout << "    " << char(186) << "   Zyma   " << char(186) << "   Vesna   " << char(186) << "   Lito    "
+		<< char(186) << "    Osin   " << char(186) << endl;
+	cout << endl;
+	cout << "    " << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(202) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(202) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(202) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205)
+		<< char(205) << char(205) << char(205) << char(205) << char(205) << char(188) << endl;
 	return 0;
 }
