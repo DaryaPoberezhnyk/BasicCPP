3f5f17e02f9722ee64c47581d74735417f3f56dd
DashaPoberezhnik <dasosha2017@gmail.com>
2022-10-11 01:35:26 +0300

    DOnt done

--- main.cpp
+++ main.cpp
@@ -1,50 +1,138 @@
-#include <iostream> 
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
 #include <ctime>
 using namespace std;
 int main()
 {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    char switch_on;
+    do {
+        system("cls");
+        cout << "#>----------<MENU>----------<#" << endl;
+        cout << "|         1 - Case 1         |" << endl;
+        cout << "|         2 - Case 2         |" << endl;
+        cout << "|         3 - Case 3         |" << endl;
+        cout << "#>--------------------------<#" << endl;
+        cout << "|         0 - Exit           |" << endl;
+        cout << "#>--------------------------<#" << endl;
+        switch_on = _getch();
+        system("cls");
+        switch (switch_on)
+        {
+        case '1':
+        {
+            int col, row, sign;
+            cout << endl;
+            cout << "Enter the height of the rectangle : ";
+            cin >> col;
+            cout << "Enter the length of the rectangle : ";
+            cin >> row;
+            cout << "Enter the sign : ";
+            cin >> sign;
+            cout << endl;
+
+            for (size_t i = 0; i <= col; i++)
+            {
+                for (size_t j = 0; j <= row; j++)
+                {
+                    cout << sign;
+                }
+                cout << endl;
+            }
+            system("pause");
+        }break;
+        case '2':
+        {
+            int col, row, sign;
+            int j;
+            int colour;
+            cout << endl;
+            cout << "Enter the height of the rectangle : ";
+            cin >> col;
+            cout << "Enter the length of the rectangle : ";
+            cin >> row;
+            cout << "Enter the sign : ";
+            cin >> sign;
+            cout << "Enter the colour : ";
+            cin >> colour;
+            cout << endl;
+
+            for (size_t i = 0; i < col; i++)
+            {
+                if (i == 0 || i == col - 1)
+                {
+                    for (size_t j = 0; j < row; j++)
+                    {
+                        if (i == 0)
+                        {
+                            if (j == 0)
+                                cout << sign;
+                            else if (j == row - 1)
+                                cout << sign;
+                            else
+
+                                cout << sign << sign;
+                        }
+                        if (i == col - 1)
+                        {
+                            if (j == 0)
+                                cout << sign;
+                            else if (j == row - 1)
+                                cout << sign;
+                            else
+                                cout << sign << sign;
+                        }
+                    }
+                }
+                else {
+                    for (size_t j = 0; j < row; j++)
+                    {
+                        if (j == 0 || j == row - 1)
+                        {
+                            cout << sign;
+                        }
+                        else {
+                            cout << "  ";
+                        }
+                    }
+                }
+                cout << endl;
+            }
+            system("pause");
+        }break;
+        case '3':
+        {
+            int one, two, three, four, five, six, r;
+            cout << endl;
+
+            cout << "  Enter the firm`s profit for the first month : ";
+            cin >> one;
+            cout << "  Enter the firm`s profit for the second month : ";
+            cin >> two;
+            cout << "  Enter the firm`s profit for the third month : ";
+            cin >> three;
+            cout << "  Enter the firm`s profit for the fourth month : ";
+            cin >> four;
+            cout << "  Enter the firm`s profit for the fifth month : ";
+            cin >> five;
+            cout << "  Enter the firm`s profit for the sixth month : ";
+            cin >> six;
+            cout << endl;
+            cout << "  The company`s profit for 6 months is : ";
+            cout << (one + two + three + four + five + six) ? cout << " GRN " << endl : cout << endl;
+            system("pause");
+        }break;
+        case '4':
+        {
+
+            
+        }break;
+        case '5':
+        {
 
 
-
-    //system("color 0A");
-    //srand(time(0));
-    //const uint32_t size = 25;
-    //long long arr[size];
-    //for (int i = 0; i < size; i++)
-    //{
-    //    arr[i] = -5 + rand() % 50;
-    //}
-    //for (int i = 0; i < size; i++)
-    //{
-    //    cout << arr[i] << "  ";
-    //}
-    //cout << endl;
-    ////------------------<SUM>-------------------    
-    ///*long long sum = 0;    
-    //for (size_t i = 0; i < size; i++)  
-    //{        sum += arr[i];    }   
-    //cout << "Sum = " << sum<< endl;*/ 
-    ////------------------<AVG>-------------------  
-    ///*double sum = 0;  
-    //for (size_t i = 0; i < size; i++)  
-    //{        sum += arr[i];    }   
-    //cout << "Avg = " << sum / size << endl;*/  
-    //long long max, min;
-    //max = min = arr[0];
-    //for (size_t i = 0; i < size; i++)
-    //{
-    //    //------------------<MIN>-------------------     
-    //    if (min > arr[i])
-    //    {
-    //        min = arr[i];
-    //    }
-    //    //------------------<MAX>-------------------    
-    //    if (max < arr[i])
-    //    {
-    //        max = arr[i];
-    //    }
-    //}
-    //cout << "Min = " << min << endl;
-    //cout << "Max = " << max << endl;
-    return 0;
+        }break;
+    }
+    } while (true);
 }
