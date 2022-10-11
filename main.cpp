e06c05ee0e6b42902be7444d4ed55639cc426abd
DashaPoberezhnik <dasosha2017@gmail.com>
2022-10-11 22:08:04 +0300

    no mister fish

--- main.cpp
+++ main.cpp
@@ -3,5 +3,65 @@ using namespace std;
 
 int main()
 {
+	const int col = 5;
+	const int row = 5;
+	int arr_2[col][row];
+	for (size_t i = 0; i < col; i++)
+	{
+		for (size_t j = 0; j < row; j++)
+		{
+			arr_2[i][j] = rand() % 10;
+			cout << "  " << arr_2[i][j] << "\t";
+		}
+		cout << endl << endl;
+	}
+	int sum = 0;
+
+	for (size_t i = 0; i < col; i++)
+	{
+		
+		for (size_t j = 0; j < row; j++)
+		{
+			sum += arr_2[i][j];
+		}
+		
+	}
+	cout << " Sum : " << sum << endl;
+	cout << endl;
+	double a = 0;
+	
+	for (size_t i = 0; i < col; i++)
+	{
+		
+		for (size_t j = 0; j < row; j++)
+		{
+			
+			a = sum / 25;
+
+		}
+	}
+	cout << " Arithmetic mean : " << a << endl;
+	cout << endl;
+	int min, max;
+	min = max = arr_2[0][0];
+	for (size_t i = 0; i < col; i++)
+	{
+		for (size_t j = 0; j < row; j++)
+		{
+			if (min > arr_2[i][j])
+			{
+				min = arr_2[i][j];
+
+			}
+			if (max < arr_2[i][j])
+			{
+				max = arr_2[i][j];
+
+			}
+		}
+	}
+	cout << " Min : " << min << endl;
+	cout << " Max : " << max << endl;
+	return 0;
 	return 0;
 }
