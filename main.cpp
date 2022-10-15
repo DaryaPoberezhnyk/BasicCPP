70234b742667a638d90bee0c587f6d69704ee077
DashaPoberezhnik <dasosha2017@gmail.com>
2022-10-15 21:50:31 +0300

    no

--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
-
+	char switch_on;
+	do {
+		system("cls");
+		cout << "#>----------<MENU>----------<#" << endl;
+		cout << "|         1 - Case 1         |" << endl;
+		cout << "|         2 - Case 2         |" << endl;
+		cout << "#>--------------------------<#" << endl;
+		cout << "|         0 - Exit           |" << endl;
+		cout << "#>--------------------------<#" << endl;
+		switch_on = _getch();
+		system("cls");
+		switch (switch_on)
+		{
+		case '1':
+		{
+		}break;
+		}
+	}
 	return 0;
 }
