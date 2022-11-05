046b8e8513f8ec619f05f24add6d8578e1ba462f
DashaPoberezhnik <dasosha2017@gmail.com>
2022-11-05 17:00:35 +0200

    DONE 25.10.22 FOR LABS

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ void stypid(T arr[], T a, const uint32_t size)
 	cout << " Sum all clients : " << a << " " << endl;
 }
 template <typename T>
-void stypid1(T arr[], T a, const uint32_t size)
+void stypid1(T arr[], T a, const uint32_t size, T arrA[], T c)
 {
 	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 11));
 	a = 0;
@@ -32,14 +32,9 @@ void stypid1(T arr[], T a, const uint32_t size)
 	{
 		arr[i] = a;
 		a++;
-
 		cout << a << " ";
 	}
 	cout << endl << endl;
-}
-template <typename T>
-void packege(T arrA[], const uint32_t size, T c)
-{
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 11));
 	c = 0;
@@ -63,31 +58,227 @@ void packege(T arrA[], const uint32_t size, T c)
 	cout << "--------------------------------------------------------------------------------------------------------------------------------------------";
 	cout << endl << endl;
 }
-template <typename T>
 
-void suma(T arrA[], const uint32_t size, T c, T sum)
+template <typename T>
+void packege(T arr[], T a,T arrA[], const uint32_t size)
 {
-	int count = 0;
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 11));
+	a = 0;
+	cout << "-----------------------------------------------------------| Packege Distribution |---------------------------------------------------------";
+	cout << endl << endl;
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 8));
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = a;
+		a++;
+		cout << a << " ";
+	}
+	cout << endl << endl;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 11));
+	int c = 0;
+	int c1 = 0;
+	int c2 = 0;
+	int c3 = 0;
+	int c4 = 0;
+	int c0 = 0;
+	int p = 0;
+	int m = 0;
+	cout << "| | | | | | | | |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |";
+	cout << endl << endl;
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 8));
+
 	for (size_t i = 0; i < size; i++)
 	{
 		arrA[i] = rand() % 5;
 		c += 1;
-	}
-	for (int i = 0; i <50; i++)
-	{
+		if (arrA[i] == 0)
+		{	
+			c0++;
+			m += 300;
+		}
+		if (arrA[i] == 1)
+		{		
+			c1++;
+			m += 100;
+		}
+	
+		if (arrA[i] == 2)
+		{
+			c2++;
+			m += 130;
+		}
 
-		int x = 0;
-		if (x == arrA[i])
-			count++;
+		if (arrA[i] == 3)
+		{			
+			c3++;
+			m += 178;
+		}
+
+		if (arrA[i] == 4) 
+		{		
+			c4++;
+			m += 230;
+		}
+		if (c > 9)
+		{
+			cout << " " << arrA[i] << " ";
+			SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 8));
+		}
+		else
+		{
+			cout << arrA[i] << " ";
+			SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 8));
+		}
 	}
-	cout << " The company`s monthly income : " ;
-	cout << count << endl;
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 11));
+	cout << endl << endl;
+	cout << "--------------------------------------------------------------------------------------------------------------------------------------------";
+	cout << endl << endl;
+
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+	if (c0 > c1 && c0 > c2 && c0 > c3 && c0 > c4) {
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " The most popular package ";
+		cout << "0 - ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 3));
+		cout << c0;
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " clients use it" << endl;
+	}
+	if (c1 > c0 && c1 > c2 && c1 > c3 && c1 > c4) {
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " The most popular package ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 3));
+		cout << "1 ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+		cout << "--> ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 3));
+		cout << c1;
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " clients use it" << endl;
+	}
+	if (c2 > c0 && c2 > c1 && c2 > c3 && c2 > c4) {
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " The most popular package ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 3));
+		cout << "2 ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+		cout << "--> ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 3));
+		cout << c2;
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " clients use it" << endl;
+	}
+	if (c3 > c0 && c3 > c1 && c3 > c2 && c3 > c4) {
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " The most popular package ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 3));
+		cout << "3 ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+		cout << "--> ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 3));
+		cout << c3;
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " clients use it" << endl;
+	}
+	if (c4 > c0 && c4 > c1 && c4 > c2 && c4 > c3)
+	{
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " The most popular package ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 3));
+		cout << "4 ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+		cout << "--> ";
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 3));
+		cout << c4;
+		SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+		cout << " clients use it" << endl;
+	}
+	cout << endl;
+}
+template <typename T>
+void packege1(T arr[], T a, T arrA[], const uint32_t size)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 11));
+	a = 0;
+	cout << "-----------------------------------------------------------| Packege Distribution |---------------------------------------------------------";
+	cout << endl << endl;
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 8));
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = a;
+		a++;
+		cout << a << " ";
+	}
+	cout << endl << endl;
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 11));
+	int c = 0;
+	int c1 = 0;
+	int c2 = 0;
+	int c3 = 0;
+	int c4 = 0;
+	int c0 = 0;
+	int p = 0;
+	int m = 0;
+	cout << "| | | | | | | | |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |";
+	cout << endl << endl;
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 8));
+
+	for (size_t i = 0; i < size; i++)
+	{
+		arrA[i] = rand() % 5;
+		c += 1;
+		if (arrA[i] == 0) 
+		{
+			c0++;
+			m += 300;
+		}
+		if (arrA[i] == 1)
+		{
+			c1++;
+			m += 100;
+		}
+		if (arrA[i] == 2)
+		{
+			c2++;
+			m += 130;
+		}
+		if (arrA[i] == 3)
+		{
+			c3++;
+			m += 178;
+		}
+		if (arrA[i] == 4)
+		{
+			c4++;
+			m += 230;
+		}
+		if (c > 9) 
+		{
+			cout << " " << arrA[i] << " ";
+			SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 8));
+		}
+		else 
+		{
+			cout << arrA[i] << " ";
+			SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 8));
+		}
+	}
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 11));
+	cout << endl << endl;
+	cout << "--------------------------------------------------------------------------------------------------------------------------------------------";
+	cout << endl << endl;
+	cout << endl;
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 6));
+	cout << " The company`s monthly income : ";
+	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+	cout << m << endl;
 	cout << endl << endl;
 }
-
-
-int main() {
-
+int main()
+{
 	int a = 0;
 	int q=0, w=0, e=0, r=0, t=0;
 	int c = 0;
@@ -95,21 +286,64 @@ int main() {
 	const uint32_t size = 50;
 	int arr[size];
 	int arrA[size];
-	cout << endl;
-	stypid(arr, a, size);
-	cout << endl;
-	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 5));
-	cout << "   #--------| Packege |-------#" << endl;
-	cout << "    |                        | " << endl;
-	cout << "    |         0 - 100        | " << endl;
-	cout << "    |         1 - 130        | " << endl;
-	cout << "    |         2 - 178        | " << endl;
-	cout << "    |         3 - 230        | " << endl;
-	cout << "    |         4 - 300        | " << endl;
-	cout << "    |                        | " << endl;
-	cout << "   #--------------------------#" << endl<<endl;
-	stypid1(arr, a, size);
-	packege(arrA, size, c);
-	suma(arrA, size, c, sum);
+	char switch_on;
+	do
+	{
+		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		system("cls");
+		cout << "\t\t\t\t\t#>----------------------<MENU>-----------------------<#" << endl;
+		cout << "\t\t\t\t\t|         1 - All clients                             |" << endl;
+		cout << "\t\t\t\t\t|         2 - All clients on packages                 |" << endl;
+		cout << "\t\t\t\t\t|         3 - Profit of the enterprise for the month  |" << endl;
+		cout << "\t\t\t\t\t|         4 - The most popular package                |" << endl;
+		cout << "\t\t\t\t\t#>---------------------------------------------------<#" << endl;
+		cout << "\t\t\t\t\t|         0 - Exit                                    |" << endl;
+		cout << "\t\t\t\t\t#>---------------------------------------------------<#" << endl;
+		switch_on = _getch();
+		system("cls");
+		switch (switch_on)
+		{
+		case '1':
+		{
+			cout << endl;
+			stypid(arr, a, size);
+			cout << endl;
+			system("pause");
+			SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+		}break;
+		case '2' :
+		{
+			cout << endl;
+			SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 14));
+			cout << "   #--------| Packege |-------#" << endl;
+			cout << "    |                        | " << endl;
+			cout << "    |       0 - 100 GRN      | " << endl;
+			cout << "    |       1 - 130 GRN      | " << endl;
+			cout << "    |       2 - 178 GRN      | " << endl;
+			cout << "    |       3 - 230 GRN      | " << endl;
+			cout << "    |       4 - 300 GRN      | " << endl;
+			cout << "    |                        | " << endl;
+			cout << "   #--------------------------#" << endl << endl;
+			stypid1(arr, a, size, arrA, c);
+			SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+			system("pause");
+		}break;
+		case '3' :
+		{
+			cout << endl;
+			packege(arr, a, arrA, size);
+			SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+			system("pause");
+		}break;
+		case '4':
+		{
+			cout << endl;
+			packege1(arr, a, arrA, size);
+			SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+			system("pause");
+		}break;
+		}
+	} while (true);
+	return 0;
 	SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
 }
