a87cec2e5181bcb1875c885993037fb02fcf6280
DashaPoberezhnik <dasosha2017@gmail.com>
2022-11-18 23:24:53 +0200

    DONE 17.11.22 FOR HW

--- main.cpp
+++ main.cpp
@@ -32,7 +32,297 @@ void first(int a)
     cout << str << endl << endl;;
 }
 ///////////////////////////////////////////////////////////////////////////////////
-void second(int a, int b)
+void two(int a,int b, int c, int d, int e, int f)
+{
+    char str[100];
+    cout << endl;
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
+    cout << " Enter text : ";
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 8));
+    gets_s(str);
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '1')
+        {
+            a++;
+        }
+        if (str[i] == '2')
+        {
+            a++;
+        }
+        if (str[i] == '3')
+        {
+            a++;
+        }
+        if (str[i] == '4')
+        {
+            a++;
+        }
+        if (str[i] == '5')
+        {
+            a++;
+        }
+        if (str[i] == '6')
+        {
+            a++;
+        }
+        if (str[i] == '7')
+        {
+            a++;
+        }
+        if (str[i] == '8')
+        {
+            a++;
+        }
+        if (str[i] == '9')
+        {
+            a++;
+        }
+        if (str[i] == '0')
+        {
+            a++;
+        }
+    }
+    cout << endl;
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
+    cout << " The number of numbers : ";
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 9));
+    cout << a << endl << endl;
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == 'q') {
+            b++;
+        }
+        if (str[i] == 'w') {
+            b++;
+        }
+        if (str[i] == 'e') {
+            b++;
+        }
+        if (str[i] == 'r') {
+            b++;
+        }
+        if (str[i] == 't') {
+            b++;
+        }
+        if (str[i] == 'y') {
+            b++;
+        }
+        if (str[i] == 'u') {
+            b++;
+        }
+        if (str[i] == 'i') {
+            b++;
+        }
+        if (str[i] == 'o') {
+            b++;
+        }
+        if (str[i] == 'p') {
+            b++;
+        }
+        if (str[i] == 'a') {
+            b++;
+        }
+        if (str[i] == 's') {
+            b++;
+        }
+        if (str[i] == 'd') {
+            b++;
+        }
+        if (str[i] == 'f') {
+            b++;
+        }
+        if (str[i] == 'g') {
+            b++;
+        }
+        if (str[i] == 'h') {
+            b++;
+        }
+        if (str[i] == 'j') {
+            b++;
+        }
+        if (str[i] == 'k') {
+            b++;
+        }
+        if (str[i] == 'l') {
+            b++;
+        }
+        if (str[i] == 'z') {
+            b++;
+        }
+        if (str[i] == 'x') {
+            b++;
+        }
+        if (str[i] == 'c') {
+            b++;
+        }
+        if (str[i] == 'v') {
+            b++;
+        }
+        if (str[i] == 'b') {
+            b++;
+        }
+        if (str[i] == 'n') {
+            b++;
+        }
+        if (str[i] == 'm') {
+            b++;
+        }
+        if (str[i] == 'Q') {
+            b++;
+        }
+        if (str[i] == 'W') {
+            b++;
+        }
+        if (str[i] == 'E') {
+            b++;
+        }
+        if (str[i] == 'R') {
+            b++;
+        }
+        if (str[i] == 'T') {
+            b++;
+        }
+        if (str[i] == 'Y') {
+            b++;
+        }
+        if (str[i] == 'U') {
+            b++;
+        }
+        if (str[i] == 'I') {
+            b++;
+        }
+        if (str[i] == 'O') {
+            b++;
+        }
+        if (str[i] == 'P') {
+            b++;
+        }
+        if (str[i] == 'A') {
+            b++;
+        }
+        if (str[i] == 'S') {
+            b++;
+        }
+        if (str[i] == 'D') {
+            b++;
+        }
+        if (str[i] == 'F') {
+            b++;
+        }
+        if (str[i] == 'G') {
+            b++;
+        }
+        if (str[i] == 'H') {
+            b++;
+        }
+        if (str[i] == 'J') {
+            b++;
+        }
+        if (str[i] == 'K') {
+            b++;
+        }
+        if (str[i] == 'L') {
+            b++;
+        }
+        if (str[i] == 'Z') {
+            b++;
+        }
+        if (str[i] == 'X') {
+            b++;
+        }
+        if (str[i] == 'C') {
+            b++;
+        }
+        if (str[i] == 'V') {
+            b++;
+        }
+        if (str[i] == 'B') {
+            b++;
+        }
+        if (str[i] == 'N') {
+            b++;
+        }
+        if (str[i] == 'M') {
+            b++;
+        }
+    }
+    cout << endl;
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
+    cout << " The number of letters : ";
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 9));
+    cout << b << endl << endl;
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '.') {
+            c++;
+        }
+    }
+    cout << endl;
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
+    cout << " The number of dots : ";
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 9));
+    cout << c << endl << endl;
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == ',') {
+            d++;
+        }
+    }
+    cout << endl;
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
+    cout << " The number of coms : ";
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 9));
+    cout << d << endl << endl;
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '!') {
+            e++;
+        }
+    }
+    cout << endl;
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
+    cout << " The number of exclamation marks : ";
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 9));
+    cout << e << endl << endl;
+    for (size_t i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '?') {
+            f++;
+        }
+    }
+    cout << endl;
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
+    cout << " The number of question marks : ";
+    SetConsoleTextAttribute(handle, WORD(0 << 4 | 9));
+    cout << f << endl << endl;
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+void third(int a)
+{
+        char str[25];
+        cout << endl;
+        HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
+        cout << " Enter text : ";
+        SetConsoleTextAttribute(handle, WORD(0 << 4 | 8));
+        gets_s(str);
+        for (size_t i = 0; i < strlen(str); i++)
+        {
+            if (str[i] == ' ') {
+                a++;
+            }
+        }
+        cout << endl;
+        SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
+        cout << " The number of spaces : ";
+        SetConsoleTextAttribute(handle, WORD(0 << 4 | 9));
+        cout << a << endl << endl;
+}
+///////////////////////////////////////////////////////////////////////////////////
+void four(int a, int b)
 {
     HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(handle, WORD(0 << 4 | 14));
@@ -92,10 +382,25 @@ int main()
         }break;
         case '2':
         {
-            int a = 0,b=0;
-            second(a,b);
+            int a = 0, b=0, c=0, d=0, e=0, f=0;
+            two(a,b,c,d,e,f);
             SetConsoleTextAttribute(handle, WORD(0 << 4 | 7));
             system("pause");
+        }break;
+        case '4':
+        {
+            int a = 0,b=0;
+            four(a,b);
+            SetConsoleTextAttribute(handle, WORD(0 << 4 | 7));
+            system("pause");
+        }break;
+        case '3':
+        {
+            int a = 0;
+            third(a);
+            SetConsoleTextAttribute(handle, WORD(0 << 4 | 7));
+            system("pause");
+
         }break;
         }
     } while (true);
