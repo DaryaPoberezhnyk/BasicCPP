70fedf4877122e258b734243058e2c599db9a83d
DashaPoberezhnik <dasosha2017@gmail.com>
2022-12-18 21:28:01 +0200

    no

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+
 using namespace std;
 
 int main()
 {
+	char path[] = "file.txt";
+	FILE* file;
+	if ((file = fopen(path, "w")) == NULL)
+	{
+		cout << " ERORR" << endl;
+
+	}
+	else
+	{
+		fputs   
+	}
 	return 0;
 }
