74e6f9407bbececdd11187931bc5d038f817546b
DashaPoberezhnik <dasosha2017@gmail.com>
2023-02-20 15:01:23 +0200

    no

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	return 0;
+struct Student {
+    string surname;
+    string group;
+    int marks[5];
+};
+
+int main() {
+    Student* students = new Student[10]; 
+    delete[] students; 
+    for (int i = 0; i < size; i++) {
+        int excellent_count = 0;
+        for (int j = 0; j < 5; j++) {
+            if (students[i].grades[j] >= 90) {
+                excellent_count++;
+            }
+        }
+        if (excellent_count * 100 / 5 > 75) {
+            cout << students[i].surname << endl;
+        }
+    }
+    return 0;
 }
